21cba8d357d6c9f9fa247aba4182c4f08f86c701
cmoody2 <cmoody.dev@gmail.com>
2020-10-17 23:53:24 -0700

    update_portfolio_website
    added all requirements for end of JavaScript assignment

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,17 +1,31 @@
-function validateForm() {
-    var a = document.forms["contactForm"]["name"].value;
-    var b = document.forms["contactForm"]["email"].value;
-    var c = document.forms["contactForm"]["phoneNumber"].value;
-    if (a == "") {
-        alert("please fill out name");
-        return false;
-    }
-    if (b == "") {
-        alert("please fill out email");
-        return false;
-    }
-    if (c == "") {
-        alert("please fill out phone number");
-        return false;
-    }
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+var slideIndex = 1;
+    showSlides(slideIndex);
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+        }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
 }
